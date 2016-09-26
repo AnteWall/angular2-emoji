bc52ddddd02a15ac900593474e7d3df71d67e8ac
Ante Wall <antewall@gmail.com>
2016-09-26 19:49:23 +0200

    Basic input emoji replacer

--- src/lib/input/input.ts
+++ src/lib/input/input.ts
@@ -1,20 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import emojis from '../db/emoji.json';
+import { EmojiUtil } from '../util/util';
 
 @Component({
   selector: 'emoji-input',
   templateUrl: 'input.html',
   styleUrls: ['input.scss']
 })
-export class EmojiInputComponent {
+export class EmojiInputComponent implements OnInit {
   input: any;
+  emojiUtil: EmojiUtil = new EmojiUtil();
 
   ngOnInit() {
     console.log(emojis);
   }
 
   onChange(newValue) {
-    console.log(newValue);
+    this.input = this.emojiUtil.emojify(newValue);
   }
 }
--- src/lib/util/util.ts
+++ src/lib/util/util.ts
@@ -1,6 +1,30 @@
 import { Injectable } from '@angular/core';
 
+import emojis from '../db/emoji.json';
+
+const PARSE_REGEX = /:([a-zA-Z0-9_\-\+]+):/g;
+
 @Injectable()
 export class EmojiUtil {
+
+  public get(emoji) {
+    for (let data of emojis) {
+      for (let e of data.aliases) {
+        if (emoji === e) {
+          return data.emoji;
+        }
+      }
+    }
+    return emoji;
+  }
+
+
+  public emojify(str) {
+    return str.split(PARSE_REGEX).map((emoji, index) => {
+      // Return every second element as an emoji
+      if (index % 2 === 0) { return emoji; }
+      return this.get(emoji);
+    }).join('');
+  }
 
 }
