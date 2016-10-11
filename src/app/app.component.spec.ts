79ce68f285891e31282f5089c1cc27ec464a59e5
Ante Wall <antewall@gmail.com>
2016-10-11 08:11:46 +0200

    Started on tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,13 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { EmojiModule } from '../lib';
+import {FormsModule} from '@angular/forms';
 
 describe('App: Angular2Emoji', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [EmojiModule, FormsModule],
       declarations: [
         AppComponent
       ],
@@ -18,16 +21,16 @@ describe('App: Angular2Emoji', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as variable pressedCount and equal 0`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.pressedCount).toEqual(0);
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render textcontent in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('emoji-input');
   }));
 });
